96ce0b3ce20593783f6cc1fc7d1bc6cc61dbb6bc
github-ci <you@example.com>
2025-06-11 00:02:30 +0800

    修正过时用法

--- src/Controller/ProcessScheduledController.php
+++ src/Controller/ProcessScheduledController.php
@@ -5,7 +5,7 @@ namespace Tourze\SMTPMailerBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Tourze\SMTPMailerBundle\Service\SMTPMailerService;
 
 /**
